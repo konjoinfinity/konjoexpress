911f335c3d341a058f92862a4cb42cbfc1881030
Wesley Scholl <clients@konjoweb.com>
2019-02-26 14:50:46 -0500

    updated community controller to include edit, update, and delete functionality

--- controllers/community.js
+++ controllers/community.js
@@ -18,5 +18,45 @@ module.exports = {
     Community.findById(req.params.id).then(community => {
       res.render("community/show", { community });
     });
+  },
+  edit: function(req, res) {
+    Community.findById(req.params.id).then(community => {
+      res.render("community/edit", { community });
+    });
+  },
+
+  /*
+  name: "Express Developers",
+  description: "Meet other express developers",
+  users: [],
+  meets: [],
+  date: Date.now()
+  */
+  update: function(req, res) {
+    const { name, description } = req.body;
+
+    Community.findOneAndUpdate(
+      req.params.id,
+      {
+        title,
+        description,
+        date: Date.now()
+      },
+      {
+        runValidators: true
+      }
+    )
+      .then(community => {
+        res.redirect(`/community/${community._id}`);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  delete: function(req, res) {
+    Community.remove({ _id: req.params.id }).then(community => {
+      console.log(community);
+      res.redirect("/");
+    });
   }
 };
