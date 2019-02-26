e5885e3c02b90c865d854ce1c0ebcf62c83cb9c9
Wesley Scholl <clients@konjoweb.com>
2019-02-26 13:49:04 -0500

    added / route, index/layout.hbs, and initialized npm. Installed express and hbs, tested route, functional

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.listen(4000, () => console.log("listening on port 4000"));
