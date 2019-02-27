df73b374c018aefc651a50bee537c0650d13e4b4
Wesley Scholl <clients@konjoweb.com>
2019-02-26 19:22:57 -0500

    installed method-override and configured for app usage

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 app.set("view engine", "hbs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.use(require("./routes/index.js"));
 
