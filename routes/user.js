396544d3d56952cadeac72dc7dd0a64ec1d84646
Wesley Scholl <clients@konjoweb.com>
2019-02-27 13:07:23 -0500

    added user routes

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user");
+
+router.get("/new", userController.new);
+router.post("/", userController.create);
+router.get("/:id", userController.show);
+router.get("/:id/edit", userController.edit);
+router.put("/:id", userController.update);
+router.delete("/:id", userController.delete);
+
+module.exports = router;
