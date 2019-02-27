7125094a98d89be2abb225f019c76d0c5e3292e3
Wesley Scholl <clients@konjoweb.com>
2019-02-27 17:27:45 -0500

    adjusted user routes for authentication

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user");
 
-router.get("/new", userController.new);
-router.post("/", userController.create);
-router.get("/:id", userController.show);
-// router.get("/:id/edit", userController.edit);
-// router.put("/:id", userController.update);
-// router.delete("/:id", userController.delete);
+router.get("/signup", userController.getSignup);
+router.post("/signup", userController.postSignup);
+
+router.get("/login", userController.getLogin);
+router.post("/login", userController.postLogin);
+
+router.get("/logout", userController.getLogout);
 
 module.exports = router;
